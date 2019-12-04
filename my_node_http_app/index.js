4374715aa785312257e5909f2abb5a7d36558486
Anita Lipsky <anita@purplebugs.com>
2019-12-04 21:56:47 +0100

    Force node app to stay running, remove not needed http library, catch errors

--- my_node_http_app/index.js
+++ my_node_http_app/index.js
@@ -1,14 +1,7 @@
-var http = require("http"); // Using built-in http node library
-const fetch = require('node-fetch'); // Using module from npm
+const fetch = require("node-fetch"); // Using module from npm
 
 // To run: node index.js
 console.log('Hello World!');
-
-// Using built-in http node library, however it is a bit low level for my needs
-
-http.get("http://localhost:9200", (response) => {
-    console.log(response.statusCode);
-});
 
 // Using npm module which is simpler to use
 fetch("http://localhost:9200")
@@ -17,4 +10,11 @@ fetch("http://localhost:9200")
     })
     .then((body) => {
         console.log(body)
+    }).catch((error) => {
+        console.log(error);
     });
+
+setInterval(() => {
+    // Keep process alive, ping every 5 seconds
+    //console.log("hello")
+},5000)
